23ae8e32c6c02351b7e1916aa5fe05f812ffd149
ashleyyyj88 <yj387@cornell.edu>
2023-05-02 22:03:27 -0400

    fixed column names

--- utilities.py
+++ utilities.py
@@ -14,10 +14,10 @@ def filterout(df, column, value):
 
 
 def get_common_dates(df_list, filenames):
-    benchmark = df_list[0]['DATE'].tolist()
+    benchmark = df_list[0]['Date'].tolist()
     common_dates = {fn: [] for fn in filenames}
     i = 0
     for df in df_list:
-        common_dates[filenames[i]] = df['DATE'].isin(benchmark)
+        common_dates[filenames[i]] = df['Date'].isin(benchmark)
         i += 1
     return common_dates
