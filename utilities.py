30f6b5d3b8fb8aa3729380ba735f0c54dfcc4831
ashleyyyj88 <yj387@cornell.edu>
2023-05-02 22:42:20 -0400

    fixed common dates

--- utilities.py
+++ utilities.py
@@ -14,10 +14,13 @@ def filterout(df, column, value):
 
 
 def get_common_dates(df_list, filenames):
-    benchmark = df_list[0]['Date'].tolist()
+
     common_dates = {fn: [] for fn in filenames}
-    i = 0
-    for df in df_list:
-        common_dates[filenames[i]] = df['Date'].isin(benchmark)
-        i += 1
+    common_dates['IG'] = df_list['IG']['Date'].isin(
+        df_list['HY']['Date']) & df_list['IG']['Date'].isin(df_list['SPX']['Date'])
+    common_dates['HY'] = df_list['HY']['Date'].isin(
+        df_list['IG']['Date']) & df_list['HY']['Date'].isin(df_list['SPX']['Date'])
+    common_dates['SPX'] = df_list['SPX']['Date'].isin(
+        df_list['HY']['Date']) & df_list['SPX']['Date'].isin(df_list['IG']['Date'])
+
     return common_dates
